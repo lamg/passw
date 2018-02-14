cdbc98ce1ad88781eb462ce6b4f2485dfeca932a
Luis Ángel Méndez Gort <gort.andres000@gmail.com>
2018-02-13 23:23:59 -0500

    It works

--- main.go
+++ main.go
@@ -1,31 +1,105 @@
 package main
 
 import (
+	"bytes"
 	"flag"
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"os"
+	"os/exec"
 	"time"
 )
 
-type PsFile []rPass
-
-type RPass struct {
-	Res string `json:"res"`
-	Ps  []Pass `json:"ps"`
-}
+type PsFile map[string][]Pass
 
 type Pass struct {
-	Date     time.Time `json:"date"`
-	Password string    `json:"password"`
+	Date     time.Time `yaml:"date"`
+	Password string    `yaml:"password"`
 }
 
 func main() {
 	var res, fl string
-	var prn, rw, ls bool
+	var erm, c, d, ls bool
 	flag.StringVar(&res, "r", "", "Resource name")
-	flag.BoolVar(&prn, "e", false,
+	flag.BoolVar(&erm, "e", false,
 		"Generate an easy to remind password")
-	flag.BoolVar(&rw, "c", false,
+	flag.BoolVar(&c, "c", false,
 		"Create new password for resource")
-	flag.BoolVar(&ls, "l", "false", "List resources")
+	flag.BoolVar(&ls, "l", false, "List resources")
 	flag.BoolVar(&d, "d", false, "Delete resource")
-	flag.StringVar(fl, "f", "", "Filename")
+	flag.StringVar(&fl, "f", "", "Filename")
+	flag.Parse()
+
+	var pf PsFile
+	var e error
+	if fl != "" {
+		pf = make(map[string][]Pass)
+		_, e = os.Stat(fl)
+		var bs []byte
+		if e == nil {
+			cmd := exec.Command("gpg", "--decrypt", fl)
+			bs, e = cmd.Output()
+		}
+		if e == nil {
+			e = yaml.Unmarshal(bs, pf)
+		}
+		if os.IsNotExist(e) {
+			e = nil
+		}
+	} else {
+		e = fmt.Errorf("No file specified")
+	}
+	if e == nil && c {
+		// create resource
+		_, ok := pf[res]
+		if !ok {
+			pf[res] = make([]Pass, 0)
+		}
+		p := Pass{Date: time.Now()}
+		var cmd *exec.Cmd
+		if erm {
+			cmd = exec.Command("apg", "-n", "1")
+		} else {
+			cmd = exec.Command("apg", "-m", "16", "-a", "1", "-n", "1")
+		}
+		bs, e := cmd.Output()
+		if e == nil {
+			p.Password = string(bs)
+			pf[res] = append(pf[res], p)
+			bs, e = yaml.Marshal(pf)
+		}
+		if e == nil {
+			// write to file
+			cme := exec.Command("gpg", "--encrypt",
+				"--default-recipient-self", "--armor", "--output", fl)
+			cme.Stdin = bytes.NewBuffer(bs)
+			e = cme.Run()
+		}
+	} else if e == nil && d {
+		// delete resource
+		delete(pf, res)
+		// write to file
+	} else if e == nil {
+		// get resource
+		p, ok := pf[res]
+		var bs []byte
+		if ok && !ls {
+			// get last password of resource
+			bs, e = yaml.Marshal(p[len(p)-1])
+		} else if ok && ls {
+			// list all passwords of resource
+			bs, e = yaml.Marshal(p)
+		} else {
+			e = fmt.Errorf("%s not found", res)
+		}
+		if e == nil {
+			fmt.Println(string(bs))
+		}
+	}
+	ex := 0
+	if e != nil {
+		fmt.Fprintln(os.Stderr, e.Error())
+		ex = 1
+	}
+	os.Exit(ex)
 }
